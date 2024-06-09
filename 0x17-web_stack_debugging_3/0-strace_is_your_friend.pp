df119dff1154ef0eb0026a149d70c14e857788db
fordstile <masterland9772@gmail.com>
2024-06-09 18:10:02 +0000

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# fix 500 error 
-exec { 'wordpress typo error':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path     => '/usr/bin:/usr/sbin:/bin',
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
